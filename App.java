58d16baf8c485fea9ed9c3d793681b46440a28eb
aliciaibanez <alicia.ibanez.escudero@gmail.com>
2025-08-22 08:34:25 +0200

    Agrego un menú para elegir la operacion a realizar

--- App.java
+++ App.java
@@ -1,14 +1,51 @@
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         InputReader inputReader = new InputReader();
+        Scanner scanner = new Scanner(System.in);
 
         int num1 = inputReader.readNumber();
         int num2 = inputReader.readNumber();
-
-        int resultado = calculator.divide(num1, num2);
-        System.out.println("Resultado de la division: " + resultado);
+        int opcion = 0;
+        do {
+            System.out.println("\nMenú:");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicación");
+            System.out.println("4. Division");
+            System.out.println ("5. Cancelar operaión");
+            System.out.println("¿Qué operación deseas realizar?");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir el salto de línea
+            int resultado = 0; 
+            switch (opcion) {
+                case 1:
+                    resultado = calculator.suma(num1, num2);
+                    System.out.println("Resultado de la suma es: " + resultado);
+                    break;
+                case 2:
+                    resultado = calculator.resta(num1, num2);
+                    System.out.println("Resultado de la resta es: " + resultado);
+                    break;
+                case 3:
+                    resultado = calculator.multiplica(num1, num2);
+                    System.out.println("Resultado de la multiplicacion es: " + resultado);
+                    break;
+                case 4:
+                    resultado = calculator.divide(num1, num2);
+                    System.out.println("Resultado de la división es: " + resultado);
+                    break;
+                case 5:
+                    System.out.println("Adiós");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        } while (opcion != 5);
 
     }
 }
--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,17 @@ public class Calculator {
     {
         return num1 / num2;
     }
+    
+    public int suma (int num1, int num2) {
+        return num1 + num2;
+    }
+    
+    public int resta (int num1, int num2) {
+        return num1 - num2;
+    }
+    
+    public int multiplica (int num1, int num2) {
+        return num1 * num2;
+    }
 }
 
